528b560a5ba0b245db07f3a80fffa75ed9a0edb6
Peter Svensson <psvensson@gmail.com>
2018-08-21 12:10:14 +0200

    scaffolding core stuff

---
+++ index.js
@@ -0,0 +1,13 @@
+class BreakoutManager
+{
+  static transformLinks(manifest, htmltext) {
+    // Detect if bower or npm manifest
+
+    // Find all script tags or import links
+
+    // Transform each relative path into ful URI that links to the source repository
+
+    // return transformed HTML text
+    
+  }
+}
