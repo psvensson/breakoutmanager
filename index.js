8c3f34a2437eb2b70185018090654bc49fb61057
Peter Svensson <psvensson@gmail.com>
2018-08-21 18:14:36 +0200

    scaffolding core stuff

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 class BreakoutManager
 {
-  static transformLinks(manifest, htmltext) {
-    // Detect if bower or npm manifest
+  // The manifest either is the object inside or the url to an npm package.json or bower bower.json that defined a specific package
+  static transformLinks(manifest) {
+    // Get manifest
 
-    // Find all script tags or import links
+    // Follow the dependency tree and download all manifests, building a hashtable of all resource names and their base URLs
 
-    // Transform each relative path into ful URI that links to the source repository
+    // For the original manifest
+
+    // Identify main file to load
+
+    // Transform each relative path into full URI that links to the source repository of the referenced resource
+    
+    // For each resource file, repeat the above, recursively for any other files referenced et.c. until we reach the end of the tree.
 
     // return transformed HTML text
-    
+
   }
 }
